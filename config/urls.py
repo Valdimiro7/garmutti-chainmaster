9ec08366e0b3f5258e50719990207a2899f4a48a
Valdimiro7 <168716332+Valdimiro7@users.noreply.github.com>
2026-03-09 12:01:05 +0000

    live test view

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.http import HttpResponse
+
+
+def live_test(request):
+    return HttpResponse("LIVE TEST OK")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("live-test/", live_test),
     path("", include("procurement.urls")),
 ]
